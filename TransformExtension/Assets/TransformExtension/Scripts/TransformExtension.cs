4398cefc0d7b8808cc6bb783ce49b06ab041e4a7
adLucem <lois.aubree@gmail.com>
2019-03-22 21:47:38 +0100

    UPDATE: Initial commit

---
+++ TransformExtension/Assets/TransformExtension/Scripts/TransformExtension.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+namespace TransformExtension
+{
+	public static class TransformExtension
+	{
+		public static Transform[] GetChildren(this Transform transform)
+		{
+			Transform[] children = new Transform[transform.childCount];
+
+			for (int i = 0; i < transform.childCount; i++)
+			{
+				children[i] = transform.GetChild(i);
+			}
+
+			return children;
+		}
+
+		public static void Freeze(this Transform transform)
+		{
+			Transform[] children = transform.GetChildren();
+
+			foreach (Transform child in children)
+			{
+				child.SetParent(null);
+			}
+
+			transform.position = Vector3.zero;
+			transform.localScale = Vector3.one;
+			transform.rotation = Quaternion.identity;
+
+			foreach (Transform child in children)
+			{
+				child.SetParent(transform);
+			}
+		}
+	}
+}
---
+++ TransformExtension/Assets/TransformExtension/Scripts/TransformExtensionEditor.cs
@@ -0,0 +1,24 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace TransformExtension.Editor
+{
+	public static class TransformExtensionEditor
+	{
+		[MenuItem("CONTEXT/Transform/Freeze")]
+		static void FreezeMenu(MenuCommand command)
+		{
+			Transform transform = (Transform)command.context;
+			Object[] toRecord = new Object[transform.childCount + 1];
+			toRecord[0] = transform;
+
+			for (int i = 0; i < transform.childCount; i++)
+			{
+				toRecord[i + 1] = transform.GetChild(i);
+			}
+
+			Undo.RecordObjects(toRecord, "Freeze");
+			transform.Freeze();
+		}
+	}
+}
